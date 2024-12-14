9365d0bfc3f3d8d3acc6ae5c1fcd04e29d527beb
ahmed_hefow <axmedaadan484@gmail.com>
2024-12-14 21:38:54 +0300

    userModal

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,27 @@
 // impelement here login, register, password reset
+import User from "../models/userModel";
+
+export const registerUser =async(req,res)=>{
+   const {name,email,password,role}=req.body
+
+   const existUser=User.findOne({email})
+   if(existUser){
+      res.status(403)
+      throw new Error("This User already exist")
+   }
+
+   const user=User.create({name,email,password,role})
+   if(user){
+      res.status(201).json({
+         id:user.id,
+         name:user.name,
+         email:user.email,
+         password:user.password,
+         role: user.role
+      })
+   }else{
+      res.status(401)
+      console.log("invalid data")
+   }
+
+}
